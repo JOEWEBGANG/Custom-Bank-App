08d93479d830ef4249362fc1f1cb7bdf22a1fe26
Zachary White <zachwhite2001@gmail.com>
2025-04-17 22:19:12 -0400

    add initial HTML, CSS, Javascript, and my logo

---
+++ app.js
@@ -0,0 +1,58 @@
+const routes = {
+    '/login': { templateId: 'login' },
+    '/dashboard': { templateId: 'dashboard' },
+};
+
+function updateRoute() {
+    const path = window.location.pathname;
+    const route = routes[path];
+  
+    if (!route) {
+      return navigate('/login');
+    }
+  
+    const template = document.getElementById(route.templateId);
+    const view = template.content.cloneNode(true);
+    const app = document.getElementById('app');
+    app.innerHTML = '';
+    app.appendChild(view);
+}
+
+function navigate(path) {
+    window.history.pushState({}, path, path);
+    updateRoute();
+}
+
+function onLinkClick(event) {
+    event.preventDefault();
+    navigate(event.target.href);
+}
+
+window.onpopstate = () => updateRoute();
+updateRoute();
+
+async function register() {
+    const registerForm = document.getElementById('registerForm');
+    const formData = new FormData(registerForm);
+    const jsonData = JSON.stringify(Object.fromEntries(formData));
+    const result = await createAccount(jsonData);
+  
+    if (result.error) {
+      return console.log('An error occurred:', result.error);
+    }
+  
+    console.log('Account created!', result);
+}
+
+async function createAccount(account) {
+    try {
+      const response = await fetch('//localhost:5000/api/accounts', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: account,
+      });
+      return await response.json();
+    } catch (error) {
+      return { error: error.message || 'Unknown error' };
+    }
+}
